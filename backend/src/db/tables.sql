03526a7d840ae9361ba0d08344a577190d6c3177
zawa-kun <shult.0219@gmail.com>
2025-02-17 00:58:25 +0900

    feat: Add created_at clumn

--- backend/src/db/tables.sql
+++ backend/src/db/tables.sql
@@ -5,7 +5,8 @@ CREATE DATABASE qualification_manager;
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 -- qualification テーブル
