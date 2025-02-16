355f9f4535bf17ea95f2ca9e5eb03a00d7664ce9
zawa-kun <shult.0219@gmail.com>
2025-02-17 00:47:07 +0900

    feat: Add registerAPI

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,21 +1,40 @@
-// import bcyrpt from "bcrypt"; #パスワードハッシュ化するライブラリ.
+import { RequestHandler } from "express";
+import bcrypt from "bcryptjs";
+import pool from "../config/database"
+import { RegisterUserRequest, AuthUserResponse, AuthError } from "../types/authTypes";
 
-export const registerUser = (req: any, res: any) => {
-    // register logic
+//新規登録API
+export const registerUser: RequestHandler<
+    {},
+    AuthUserResponse | AuthError,
+    RegisterUserRequest
+> = async (req, res) => {
     try {
-        const User = {
-            email: req.body.email,
-            password: req.body.password,
-        }
-        // SQLへの挿入
-        const user = await User.save();
-        return res.status(200).json(user);
+        const { email, password } = req.body; 
+        
+        //　email or パスワードがない時, 登録できない.
+        if(!email || !password){ res.status(400).json({message: "メールとパスワードを入力してください。" });}
+        
+        // パスワードのハッシュ化.硬度は10.
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = await pool.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [email, hashedPassword]
+        );
+
+        // JWT token（仮）
+        const token = "sample-jwt-token";
+        
+        res.status(201).json({
+            id: newUser.rows[0].id,
+            email: newUser.rows[0].email,
+            createdAt: newUser.rows[0].created_at,
+            token
+    });
     } catch (error) {
-        return res.status(500).json
+        console.error("登録エラー:", error);
+        res.status(500).json({message: "サーバーエラー"});
     }
-    
-    
-    res.send("registerUser");
 };
 
 export const loginUser = (req: any, res: any) => {
